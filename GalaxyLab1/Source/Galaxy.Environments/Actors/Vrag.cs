8112f35f0b61527d05e84c3fb85c125aba8f8807
Asassin42 <SSKvakin@1cbit.ru>
2015-10-01 21:23:19 +0700

    GalaxyNumber1

---
+++ GalaxyLab1/Source/Galaxy.Environments/Actors/Vrag.cs
@@ -0,0 +1,30 @@
+﻿#region using
+
+using System;
+using System.Diagnostics;
+using System.Windows;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Environment;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+
+#endregion
+
+namespace Galaxy.Environments.Actors
+{
+    public class Vrag : Ship
+    {
+        public override void Load()
+        {
+            InitTimer();
+            Load(@"Assets\Vrag.png");
+        }
+
+        public Vrag(ILevelInfo info) : base(info)
+        {
+            Width = 30;
+            Height = 30;
+            ActorType = ActorType.Enemy;
+        }
+    }
+}
---
+++ GalaxyLab1/Source/Galaxy.Environments/Actors/Vrag2.cs
@@ -0,0 +1,30 @@
+﻿#region using
+
+using System;
+using System.Diagnostics;
+using System.Windows;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Environment;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+
+#endregion
+
+namespace Galaxy.Environments.Actors
+{
+  public class Vrag2 : Ship
+  {
+    public Vrag2(ILevelInfo info):base(info)
+    {
+      Width = 30;
+      Height = 30;
+      ActorType = ActorType.Enemy;
+    }
+    public override void Load()
+    {
+        InitTimer();
+        Load(@"Assets\Vrag2.png");
+    }
+ 
+  }
+}
